5546f332adefd6f0e598776ef22d6cc7319fa28f
JakWai01 <jakob.waibel@web.de>
2023-02-22 16:07:47 +0100

    feat: Validate input

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :repl-options {:init-ns clodl.core}
-  :main clodl.core/-main)
+  :main clodl.core/-main
+  :plugins [[lein-cljfmt "0.9.2"]])
