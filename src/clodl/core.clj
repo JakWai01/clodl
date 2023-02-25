f305148c52535a9c8a625b9ee2ef846a6d11d82c
JakWai01 <jakob.waibel@web.de>
2023-02-25 13:32:54 +0100

    feat: Decompose code into smaller functions

--- src/clodl/core.clj
+++ src/clodl/core.clj
@@ -6,20 +6,6 @@
   [path]
   (str/split (slurp path) #"\n"))
 
-(defn evaluate-guess
-  "Mark each char as either contained, contained and in the correct place or uncontained"
-  [chosen-word guess]
-  [(= guess chosen-word) (reduce str
-                                 (map
-                                  (fn [guess-char, chosen-word-char]
-                                    (if (= guess-char chosen-word-char)
-                                      (str "\u001B[32m" guess-char "\u001B[0m")
-                                      (if (not (nil? (str/index-of chosen-word guess-char)))
-                                        (str "\u001B[33m" guess-char "\u001B[0m")
-                                        (str guess-char))))
-                                  (seq guess)
-                                  (seq chosen-word)))])
-
 (defn valid?
   "Check that the guess is made out of valid chars and is exactly five characters long"
   [guess]
@@ -27,34 +13,59 @@
    (= (count guess) 5)
    (every? #(Character/isLetter %) guess)))
 
+(defn print-text-in-color
+  "Print the following text in the specified color"
+  [text & [color]]
+  (case color
+    "GREEN" (str "\u001B[32m" text "\u001B[0m")
+    "YELLOW" (str "\u001B[33m" text "\u001B[0m")
+    (str text)))
+
+(defn get-letter-matches
+  "Charwise comparison and colorization"
+  [guess target]
+  (map
+   #(cond
+      (= %1 %2) (print-text-in-color %1 "GREEN")
+      (not (nil? (str/index-of target %1))) (print-text-in-color %1 "YELLOW")
+      :else (print-text-in-color %1)) guess target))
+
+(defn evaluate-guess
+  "Mark each char as either contained, contained and in the correct place or uncontained"
+  [guess target]
+  [(= guess target)
+   (reduce str (get-letter-matches guess target))])
+
 (defn start-round
   "Take one guess and evaluate it's correctness"
-  [chosen-word]
+  [target]
   (print "Please enter a word: ")
   (flush)
   (let [guess (read-line)]
     (if (valid? guess)
-      (let [[won colored-guess] (evaluate-guess chosen-word guess)]
-        (println colored-guess)
-        (boolean won))
-      (start-round chosen-word))))
+      (evaluate-guess guess target)
+      ; Think about still using an exception here
+      (do (println "Invalid word, please try again. The word should have consist out of 5 letters.")
+          (start-round target)))))
 
-(defn print-color
-  "Print the following text in the specified color"
-  [color]
-  ())
+(defn get-random-word
+  "Get a random word from the wordlist"
+  [word-list]
+  (rand-nth word-list))
 
 (defn start-game
   "Play a game of wordle. A game consists out of 6 rounds."
   [path]
   (let [word-list (get-word-list-from-file path)
-        chosen-word (rand-nth word-list)]
-    (println chosen-word)
+        target (get-random-word word-list)]
+    ; Remove the target word later
+    (println target)
+    ; Do this recursively so we can send the states in between the invocations. Print summary here but the current state in the round function
     (loop [x 0]
       (when (< x 6)
-        (if (= (start-round chosen-word) false)
-          (recur (+ x 1))
-          (println "You gessed the word!"))))))
+        (let [[won colorized-guess] (start-round target)]
+          (println colorized-guess)
+          (if (not (boolean won)) (recur (inc x)) (println "You guessed the word!")))))))
 
 (defn -main
   "I don't do a whole lot."
@@ -62,5 +73,5 @@
   (case (first args)
     "play" (-> (last args)
                start-game)
-    "help" (println "\u001B[33mUsage:\ngenerate filename bitsize\nencrypt keyfile message\ndecrypt keyfile cipher\u001B[0m")
-    (println "\u001B[33m invalid commands, use help to display information.\u001B[0m")))
+    "help" (println (print-text-in-color "Usage:\nplay wordlist\nhelp" "YELLOW"))
+    (println (print-text-in-color "Invalid commands, use help to display information." "YELLOW"))))
